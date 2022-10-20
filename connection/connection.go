3ba7a81189c0bb30c870eb95e500ea3d4a3442b1
Amir Larki <amir555larki@gmail.com>
2022-10-20 20:20:59 +0330

    calculate total weight

--- connection/connection.go
+++ connection/connection.go
@@ -11,29 +11,33 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var Product *mongo.Collection
 var City *mongo.Collection
 var Province *mongo.Collection
 var Customer *mongo.Collection
 var ShippingMethod *mongo.Collection
 var Basket *mongo.Collection
-func init(){
+
+func init() {
 	godotenv.Load()
 	mongoURI := os.Getenv("MONGO_URI")
 	mongoDB := os.Getenv("MONGO_DB")
-	client , conErr := mongo.Connect(context.Background(), options.Client().ApplyURI(mongoURI))
+	client, conErr := mongo.Connect(context.Background(), options.Client().ApplyURI(mongoURI))
 	if conErr != nil {
 		fmt.Println("error in db connection")
 		os.Exit(1)
 	}
 	db := client.Database(mongoDB)
+	Product = db.Collection("products")
 	City = db.Collection("cities")
 	Province = db.Collection("provinces")
 	Customer = db.Collection("customers")
 	ShippingMethod = db.Collection("shippingMethods")
 	Basket = db.Collection("baskets")
 }
+
 // for test purpose
-func init(){
+func init() {
 	count, _ := Customer.CountDocuments(context.Background(), bson.M{})
 	if count == 0 {
 		Customer.InsertOne(context.TODO(), bson.M{"phone": "09107655173"})
--- shipping/site/providers/calculateBasketWeight.go
+++ shipping/site/providers/calculateBasketWeight.go
@@ -2,7 +2,7 @@ package providers
 
 import (
 	"context"
-	"fmt"
+	"github.com/amir5li/shipment/models"
 
 	"github.com/amir5li/shipment/connection"
 	"go.mongodb.org/mongo-driver/bson"
@@ -10,7 +10,71 @@ import (
 )
 
 func calculateBasketWeight(ctx context.Context, customerID primitive.ObjectID) uint {
-	var 
-
-	return result
+	var variationsWeight []struct {
+		VarID  primitive.ObjectID `bson:"varID"`
+		Weight uint               `bson:"weight"`
+	}
+	var targetBasket models.Basket
+	findRes := connection.Basket.FindOne(
+		ctx,
+		bson.M{"customerID": customerID},
+	)
+	findRes.Decode(&targetBasket)
+	var varIDs []primitive.ObjectID
+	for _, variation := range targetBasket.Items {
+		varIDs = append(varIDs, variation.VarID)
+	}
+	aggDecoding, _ := connection.Product.Aggregate(
+		ctx,
+		bson.A{
+			bson.M{
+				"variations._id": bson.M{
+					"$in": varIDs,
+				},
+			},
+			bson.M{
+				"$set": bson.M{
+					"varID": bson.M{
+						"$reduce": bson.M{
+							"input":        "$variations",
+							"initialValue": nil,
+							"in": bson.M{
+								"$cond": bson.M{
+									"if": bson.M{
+										"$and": bson.A{
+											bson.M{
+												"$eq": bson.A{"$$value", nil},
+											},
+											bson.M{
+												"$in": bson.A{"$$this._id", varIDs},
+											},
+										},
+									},
+									"then": "$$this._id",
+									"else": "$$value",
+								},
+							},
+						},
+					},
+				},
+			},
+			bson.M{
+				"$project": bson.M{
+					"_id":    0,
+					"weight": 1,
+					"varID":  1,
+				},
+			},
+		},
+	)
+	aggDecoding.All(ctx, &variationsWeight)
+	var totalWeight uint
+	for _, basketItem := range targetBasket.Items {
+		for _, varW := range variationsWeight {
+			if varW.VarID.Hex() == basketItem.VarID.Hex() {
+				totalWeight += basketItem.Count + varW.Weight
+			}
+		}
+	}
+	return totalWeight
 }
